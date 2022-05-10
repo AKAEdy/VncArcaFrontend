4667be000ba98003e935eb0e12758318066fba15
chrislucero19 <christianolucero@gmail.com>
2022-05-09 20:34:34 -0500

    09/05/2022 register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login.component';
 import { RegistroComponent } from './auth/registro.component';
 import { MenuComponent } from './menu/menu.component';
 import { IndexComponent } from './index/index.component';
+//import {ActiveToast, IndividualConfig, ToastrService} from 'ngx-toastr';
 
 @NgModule({
   declarations: [
